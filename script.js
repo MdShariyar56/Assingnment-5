5d1fc2ffc29b870b3d150e885db00b1a92aec8d3
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-08-29 00:56:44 +0600

    added a heart count iteams

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function(){
+  let favCount = 0;
+  const navbarCount = document.getElementById("hart-available");
+
+  for (let i = 1; i <= 9; i++) {
+    const heart = document.getElementById(`heart-${i}`);
+    if (!heart) continue;
+
+    heart.addEventListener("click", function(){
+      favCount++; 
+      navbarCount.innerText = favCount;
+    });
+  }
+});
+
